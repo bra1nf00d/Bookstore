1f2725950e6f18ee655a169e5d7d8c77c57a8c00
Vladyslav Taranenko <bra1nf00d@icloud.com>
2022-03-17 20:24:29 +0200

    prepared the project for working with UI without IB

--- Bookstore/SceneDelegate.swift
+++ Bookstore/SceneDelegate.swift
@@ -13,7 +13,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let rootViewController = ViewController()
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.makeKeyAndVisible()
+        window?.rootViewController = rootViewController
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
