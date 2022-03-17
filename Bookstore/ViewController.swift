81752ac2c7d47911b7e7280ddacbeff0865f5941
Vladyslav Taranenko <bra1nf00d@icloud.com>
2022-03-17 20:09:32 +0200

    Initial Commit

---
+++ Bookstore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bookstore
+//
+//  Created by Vladyslav Taranenko on 17.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
